833494391eefb378ac5e02e495b70f5074d580af
Mathéba <ndxendernight@gmail.com>
2026-04-12 23:08:33 +0200

    feat: init project

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      name = "pulse";
+      packages =
+        with pkgs;
+        with pkgs.ocamlPackages; [
+          ocaml
+          ocaml-lsp
+          ocamlformat
+          dune_3
+        ];
+    };
+
+    packages.${system}.pulse = pkgs.ocamlPackages.buildDunePackage (finalAttrs: {
+      pname = "pulse";
+      version = "0.1.0";
+
+      src = ./.;
+    });
+  };
+}
